99697635aacc0dd974a7ff26043cc681257d7678
emuldrew855 <edwardmuldrew@gmail.com>
2019-11-01 15:59:40 +0000

    Add post request

--- src/routes/edward/page.js
+++ src/routes/edward/page.js
@@ -1,28 +1,29 @@
 $(document).ready(() => {
 	alert('Page Ready');
-	
-var request = new XMLHttpRequest()
-
-request.open('GET', 'http://localhost:9000/v1/hello', true)
-request.onload = function() {
-  // Begin accessing JSON data here
-  //var data = XMLDocument.parse(this.response)
-  console.log(this.response);
-
-  
-}
-
-request.send()
 });
 
 var panel, request; 
 
 function init() {
+	document.getElementById("sub").onclick = submit; 
 	panel = document.getElementById("panel");
 	var btn = document.getElementById("btn");
 	btn.onclick = doRequest;
 }
 document.addEventListener("DOMContentLoaded", init, false);
+
+function submit() {
+	console.log("submit method");
+	var form = document.getElementById("userDetails");
+	var formData = JSON.stringify(Object.fromEntries(form));
+	
+	console.log(form);
+	console.log(formData);
+	form.action = "http://localhost:9000/v1"
+	form.method = "POST";
+	form.submit();
+	console.log(form.responseText);
+}
 
 function doRequest() {
 	request = new XMLHttpRequest();
@@ -37,7 +38,7 @@ function showStatus()
 	if(request.readyState === 4)
 	{
 		if(request.status === 200) {
-			panel.innerHTML += "<br> Request Succeeded" ; 
+			panel.innerHTML += "<br> " + request.responseText ; 
 		}
 	}
 }
