c3924ed875b713ee2f12577284465dea5d762d6f
EdwardMuldrew <emuldrew03@aub.ac.uk>
2019-11-19 11:29:29 +0000

    Add axios http calls to application

--- src/routes/edward/page.js
+++ src/routes/edward/page.js
@@ -1,28 +1,45 @@
+
 $(document).ready(() => {
 	alert('Page Ready');
 });
 
 var panel, request; 
 
+const axios = require('axios');
+
 function init() {
 	document.getElementById("sub").onclick = submit; 
 	panel = document.getElementById("panel");
 	var btn = document.getElementById("btn");
+	var btnPost = document.getElementById("btnPost");
+	btnPost.onclick = postRequest;
 	btn.onclick = doRequest;
 }
 document.addEventListener("DOMContentLoaded", init, false);
 
-function submit() {
+async function postRequest() {
+	//axios.post("http://localhost:9000/v1");
 	console.log("submit method");
-	var form = document.getElementById("userDetails");
-	var formData = JSON.stringify(Object.fromEntries(form));
-	
-	console.log(form);
-	console.log(formData);
-	form.action = "http://localhost:9000/v1"
-	form.method = "POST";
-	form.submit();
-	console.log(form.responseText);
+	params = {
+			"firstName": "Edward",
+			"lastName": "Muldrew"   
+      }
+	let res = await axios.post('http://localhost:9000/v1', params);
+
+    console.log(`Status code: ${res.status}`);
+    console.log(`Status text: ${res.statusText}`);
+    console.log(`Request method: ${res.request.method}`);
+    console.log(`Path: ${res.request.path}`);
+
+    console.log(`Date: ${res.headers.date}`);
+    console.log(`Data: ${res.data}`);
+//	var formData = JSON.stringify(Object.fromEntries(form));
+//	console.log(form);
+//	console.log(formData);
+//	form.action = "http://localhost:9000/v1"
+//	form.method = "POST";
+//	form.submit();
+//	console.log(form.responseText);
 }
 
 function doRequest() {
