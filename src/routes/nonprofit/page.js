0c351f07bf3ceb81e874a4cdfeaa96e558d440cc
EdwardMuldrew <emuldrew03@aub.ac.uk>
2019-12-03 19:00:28 +0000

    Add latest advanced charity search module

--- src/routes/nonprofit/page.js
+++ src/routes/nonprofit/page.js
@@ -1,13 +1,13 @@
 function init() {
 	panel = document.getElementById("panel");
 	var btn = document.getElementById("btn");
-	btn.onclick = getItemRequest;
+	btn.onclick = getNonProfitRequest;
+	document.getElementById("response").style.display = "hidden";
 }
 document.addEventListener("DOMContentLoaded", init, false);
 
-function getItemRequest() {
-	var responseText = document.getElementById('response');
-    console.log('Get Item Request')
+function getNonProfitRequest() {
+    console.log('Get Non Profit Request')
 	request = new XMLHttpRequest();
 	request.open("GET", 'http://localhost:9000/v1/FindNonProfit', true);
 	request.setRequestHeader('Content-Type', 'application/xml');
@@ -25,12 +25,35 @@ function showStatus()
 			parser = new DOMParser();
 			xmlDoc = parser.parseFromString(request.responseText,"text/xml");
 			panel.innerHTML += "<br> " + xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue + "<br>" +
-			xmlDoc.getElementsByTagName("mission")[0].childNodes[0].nodeValue +
-			xmlDoc.getElementsByTagName("description")[0].childNodes[0].nodeValue 
+			"<h3> Mission:  </h3>" + xmlDoc.getElementsByTagName("mission")[0].childNodes[0].nodeValue +
+			"<h3> Description: </h3>" + xmlDoc.getElementsByTagName("description")[0].childNodes[0].nodeValue 
 			var img = 	xmlDoc.getElementsByTagName("logoURL")[0].childNodes[0].nodeValue;
 			var charityImage = document.getElementById("charityImage");
+			document.getElementById("response").style.display = "block";
 			charityImage.src = img;
+			getNonProfitItems();
 			console.log(request.responseText);
 		}
 	}
+}
+
+function getNonProfitItems() {
+	console.log('Get Non Profit Items');
+	request = new XMLHttpRequest();
+	request.open("GET", 'http://localhost:9000/v1/AdvancedCharityItems?charityItemId=19790&listingType=FixedPrice', true);
+	request.setRequestHeader('Content-Type', 'application/xml');
+	request.send(null);
+	request.onreadystatechange = displayNonProfitItems;
+}
+
+function displayNonProfitItems() {
+	console.log(request.responseText);
+	console.log('Display Non Profit Items Method')
+	if(request.status === 200) {
+		console.log('Request Succsess');
+		console.log(request.responseText);
+	}
+	var obj = JSON.parse(request.responseText);
+	document.getElementById("responseText").innerHTML = obj.items.items.item[0].title
+	
 }
