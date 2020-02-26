f855383bb93eccbf5a89ec7f7648c0d0ba324273
EdwardMuldrew <emuldrew03@aub.ac.uk>
2020-02-26 19:57:22 +0000

    Add latest sign up & log in pages

---
+++ src/routes/login/page.js
@@ -0,0 +1,31 @@
+var username, password; 
+
+function init() {
+	console.log('Log In Page')
+}
+document.addEventListener("DOMContentLoaded", init, false);
+
+function logIn() {
+    console.log('Log In Method');
+    username = document.getElementById("username");
+    password = document.getElementById("password");
+    request = new XMLHttpRequest();
+	request.open("GET", '	http://localhost:9000/auth/LogIn?username=' + username + '&password=' +password, true);
+	request.setRequestHeader('Content-Type', 'application/xml');
+	request.send(null);
+	request.onreadystatechange = permissionGranted;
+}
+
+function permissionGranted() {
+    if (request.readyState === 4) {
+		if (request.status === 200) {
+            if(request.response == "GrantAccess") {
+                console.log('Access granted');
+                panel.innerHTML += "Access Granted"; 
+            }else{
+                console.log('Permission denied');
+                panel.innerHTML += "Permission denied"; 
+            }
+        }
+    }
+}
