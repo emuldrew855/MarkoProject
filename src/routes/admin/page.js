99f78f4135e9b07d6dcafde7660b449322c73351
EdwardMuldrew <emuldrew03@aub.ac.uk>
2020-03-09 13:49:23 +0000

    Add latest working functionality of A/B split test

--- src/routes/admin/page.js
+++ src/routes/admin/page.js
@@ -1,13 +1,39 @@
+var userData = [];
+var numOfA,numOfB = 0;
 function init() {
+	numOfA = 0;
+	numOfB = 0;
     console.log("Admin Page")
+	request = new XMLHttpRequest();
+	request.open("GET", 'http://localhost:9000/v2/GetUserActions', true);
+	request.setRequestHeader('Content-Type', 'application/xml');
+	request.send(null);
+	request.onreadystatechange = displayData;
 }
 
-window.onload = function() {
+function displayData() {
+	var jsonString = request.response; 
+	var obj = JSON.parse(jsonString);
+	if (request.readyState === 4) {
+        if (request.status === 200) {
+			for (var i = 0; i < obj.length; i++) { 
+				userData[i] = obj[i];
+				for (let key of Object.keys(obj[i])) {
+					if(key=="A")	{
+						numOfA= numOfA + 1;
+					}else{
+						numOfB = numOfB + 1;
+					}
+				}
+			}
+	console.log(numOfA);
+	console.log(numOfB);
+	console.log(userData);
 
 var chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
 	title: {
-		text: "Did user group view page on ebay?"
+		text: "Which user group viewed page on ebay?"
 	},
 	data: [{
 		type: "pie",
@@ -15,13 +41,15 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		yValueFormatString: "##0.00\"%\"",
 		indexLabel: "{label} {y}",
 		dataPoints: [
-			{y: 60, label: "User Group A"},
-			{y: 40, label: "User Group B"},
-		]
+            {y: numOfA, label: "User Group A"},
+            {y: numOfB, label: "User Group B"},
+        ]
 	}]
 });
 chart.render();
 
+}
+}
 }
 
 document.addEventListener("DOMContentLoaded", init, false);
